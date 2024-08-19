545b2f97d7c64ee910b1b58e648a6410092923aa
Alejandro Oliveira Perrone <alejandrop2506@gmail.com>
2024-08-19 15:26:53 -0300

    v0.1

---
+++ src/components/Container.js
@@ -0,0 +1,17 @@
+const Task = (props) => {
+    return(
+        <div className="container">
+            <i></i>
+        </div>
+    )
+}
+
+const Container = (props) => {
+    return(
+        <div className="container">
+            
+        </div>
+    );
+};
+
+export default Container; 
---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import '../index.scss';
+
+const Title = () => {
+    return(
+        <div>
+            <h1 className="title" >TaskFlow</h1>
+        </div>    
+    );
+}
+
+export default Title;
