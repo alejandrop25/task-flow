d9356e6cd6fead1c5f1bc3eb48e0a535e4e7e3b4
Alejandro Oliveira Perrone <alejandrop2506@gmail.com>
2024-08-27 15:04:52 -0300

    80% funcional

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,15 +1,28 @@
+import { ReactComponent as CheckBox } from '../check-box-empty.svg';
+import { useState } from 'react';
+import '../Task.scss';
+
 const Task = (props) => {
+    const [input, setInput] = useState('');
+    function handleChange(event){
+        setInput(event.target.value);
+    }    
+    function handleSubmit(){
+        
+    }
     return(
-        <div className="container">
-            <i></i>
+        <div className="task">
+            <CheckBox />
+            <input placeHolder="Digite aqui sua tarefa..." value={input.content} onChange={handleChange} ></input>
+            <button className="btn btn-primary" onClick={handleSubmit}>Submit</button>
         </div>
     )
-}
+};
 
 const Container = (props) => {
     return(
         <div className="container">
-            
+            <Task />
         </div>
     );
 };
