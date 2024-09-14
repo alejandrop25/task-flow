74e704fb6d851e10729f9c914e7ad30ec063907d
Alejandro Oliveira Perrone <alejandrop2506@gmail.com>
2024-09-14 18:37:49 -0300

    80

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,19 +2,33 @@ import { ReactComponent as CheckBox } from '../check-box-empty.svg';
 import { useState } from 'react';
 import '../Task.scss';
 
+const NewTask = ({text}) => {
+    <div className="task" >
+        <CheckBox /> <span>{text}</span>
+    </div>
+};
+
+
 const Task = (props) => {
+    const [components, setComponents] = useState([]);
     const [input, setInput] = useState('');
     function handleChange(event){
         setInput(event.target.value);
-    }    
-    function handleSubmit(){
-        
     }
+        
+    const handleSubmit = (e) => {
+        setComponents([...components, <NewTask key={components.length} text={input} />])
+    };
     return(
+        <div>
         <div className="task">
             <CheckBox />
-            <input placeHolder="Digite aqui sua tarefa..." value={input.content} onChange={handleChange} ></input>
-            <button className="btn btn-primary" onClick={handleSubmit}>Submit</button>
+            <input placeHolder="Digite aqui sua tarefa..." value={input} onChange={handleChange} ></input>
+            <button className="btn btn-primary" type="submit" onClick={handleSubmit}>Submit</button>
+        </div>
+        <div>
+            {components}
+        </div>
         </div>
     )
 };
